605f3dd5d78b8824fe94ac6595f6cf6503408304
V3n0m1010011010 <116117724+V3n0m1010011010@users.noreply.github.com>
2024-02-19 08:12:19 +0000

    Add files via upload

---
+++ error.ps1
@@ -0,0 +1,2 @@
+Add-Type -AssemblyName System.Windows.Forms
+[System.Windows.Forms.MessageBox]::Show("Es ist ein Fehler aufgetreten!", "Fehler", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Error)
