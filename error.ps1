4d9881084d814053ecbae1502306ecbd69ce561e
V3n0m1010011010 <116117724+V3n0m1010011010@users.noreply.github.com>
2024-03-01 11:35:18 +0000

    Update error.ps1

--- error.ps1
+++ error.ps1
@@ -1,4 +1,5 @@
 Add-Type -AssemblyName System.Windows.Forms
 $Username = "`nUsername: $($env:USERNAME)"
 $netinfo = "`n$(ipconfig)"
-[System.Windows.Forms.MessageBox]::Show($Username+$netinfo, "Don't plugin random USB's", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Error)
+$endText = $Username+$netinfo
+[System.Windows.Forms.MessageBox]::Show($endText, "Don't plugin random USB's", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Error)
