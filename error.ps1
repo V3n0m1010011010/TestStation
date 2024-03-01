505888d2c7fae20a495fbaf9c008f52aab4cf06d
V3n0m1010011010 <116117724+V3n0m1010011010@users.noreply.github.com>
2024-03-01 11:40:35 +0000

    Update error.ps1

--- error.ps1
+++ error.ps1
@@ -1,5 +1,6 @@
 Add-Type -AssemblyName System.Windows.Forms
 $Username = "`nUsername: $($env:USERNAME)"
 $netinfo = "`n$(ipconfig)"
-$endText = $Username+$netinfo
+$netProfiles = "`n$(Get-NetConnectionProfile | Where-Object { $_.NetworkCategory -eq 'Private' })"
+$endText = $Username+$netinfo+
 [System.Windows.Forms.MessageBox]::Show($endText, "Don't plugin random USB's", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Error)
