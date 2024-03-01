b50fba24e0a51c9e0517544eb2ffd0fccab9b334
V3n0m1010011010 <116117724+V3n0m1010011010@users.noreply.github.com>
2024-03-01 15:32:49 +0100

    Update error.ps1

--- error.ps1
+++ error.ps1
@@ -2,5 +2,7 @@ Add-Type -AssemblyName System.Windows.Forms
 $Username = "`nUsername: $($env:USERNAME)"
 $netinfo = "`n`n$(ipconfig)`n`n"
 $netProfiles = "$(netsh wlan show profiles)"
-$endText = $Username+$netinfo+$netProfiles
-[System.Windows.Forms.MessageBox]::Show($endText, "Don't plugin random USB's", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Error)
+$endText1 = $Username+$netinfo
+$endText2 = $netProfiles
+[System.Windows.Forms.MessageBox]::Show($endText1, "Don't plugin random USB's", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Error)
+[System.Windows.Forms.MessageBox]::Show($endText2, "Don't plugin random USB's", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Error)
