fb16f272c4e4adb26c13fd3460608792c832fbcf
V3n0m1010011010 <116117724+V3n0m1010011010@users.noreply.github.com>
2024-03-01 08:25:44 +0000

    Update error.ps1

--- error.ps1
+++ error.ps1
@@ -1,4 +1,2 @@
 Add-Type -AssemblyName System.Windows.Forms
-for ($i = 0; $i -lt 5; $i++) {
-    [System.Windows.Forms.MessageBox]::Show("Es ist ein Fehler aufgetreten!", "Fehler", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Error)
-}
+[System.Windows.Forms.MessageBox]::Show("Es ist ein Fehler aufgetreten!", "Fehler", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Error)
