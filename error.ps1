3ae5b62d1c1cb1d0e359937acbcf8cb7b817d385
V3n0m1010011010 <116117724+V3n0m1010011010@users.noreply.github.com>
2024-12-15 14:42:38 +0100

    Update error.ps1

--- error.ps1
+++ error.ps1
@@ -4,4 +4,7 @@ $netinfo = "`n`n$(ipconfig | Select-String -Pattern 'IP')"
 $netProfiles = "`n`n$(netsh wlan show profiles)"
 $tree = "`n`n$(tree)"
 $endText = $Username+$netProfiles+$tree
-[System.Windows.Forms.MessageBox]::Show($endText, "Don't plugin random USB's", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Error)
+For ($i=0; $i -le 10; $i++) {
+  [System.Windows.Forms.MessageBox]::Show($endText, "Don't plugin random USB's", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Error)
+}
+#[System.Windows.Forms.MessageBox]::Show($endText, "Don't plugin random USB's", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Error)
